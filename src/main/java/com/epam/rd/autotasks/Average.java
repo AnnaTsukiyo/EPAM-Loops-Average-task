ba65f76dd698a3f5c818e1073540a779130e0cc0
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-24 15:11:52 +0200

    done

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         // Use Scanner methods to read input
-
+            int counter = 0, input = 0, large = Integer.MIN_VALUE,sum=0;
+            Scanner in = new Scanner(System.in);
+            while ((input = in.nextInt()) != 0) {
+                large = Integer.max(large, input);
+                sum += input;
+                counter++;
+            }
+            if (counter == 0) {
+                System.out.println("No data was entered.");
+            }else{
+                double average = sum/counter;
+                System.out.println((int)average);
+            }
     }
-
 }
